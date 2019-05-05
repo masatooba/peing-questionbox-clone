e89857962bffe5307668660549e3dbfde191a30b
seiyatakahashi <twitter333555@gmail.com>
2019-05-05 15:15:26 +0900

    h

--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -3,16 +3,11 @@ class BaseController < ApplicationController
   helper_method :current_user
 
   def login_required
-  @current_user = current_user
+    @current_user ||= SessionUser.new(session) if session[:user_id]
     unless @current_user
       flash[:notice] = "ログインが完了していません。"
       redirect_to root_path
     end
   end
 
-  private
-  def current_user
-    # SessionUserクラスからユーザ情報を取得するように
-    @current_user ||= SessionUser.new(session) if session[:user_id]
-  end
 end
